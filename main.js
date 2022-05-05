b1662e0333ca939bab1ca7877145eb99147444e9
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-05-05 19:45:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-song = ""
+song = "";
+left_Wrist_x = "" ;
+right_Wrist_x = "" ;
+
+left_Wrist_y = "" ;
+right_Wrist_y = "" ;
 
 function preload() {
     song = loadSound("music.mp3")
@@ -10,7 +15,29 @@ function setup() {
 
      video = createCapture(VIDEO);
      video.hide();
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on("pose",gotPosses);
+
 }
+
+ function modelLoaded() {
+     console.log('Model is Loaded');
+ }
+
+ function gotPosses(results) {
+     if (results.length>0) {
+         console.log(results);
+         left_Wrist_x = results[0].pose.leftWrist.x;
+         left_Wrist_y = results[0].pose.leftWrist.y;
+
+         right_Wrist_x = results[0].pose.rightWrist.x;
+         right_Wrist_y = results[0].pose.rightWrist.y;
+
+        console.log("Left wrist x is "+ left_Wrist_x + " and Right wrist x is "+right_Wrist_x);
+        console.log("Left wrist y is "+ left_Wrist_y + " and Right wrist y is "+right_Wrist_y);
+     }
+ }
 
  function draw() {
      image(video,0,0,600,600);
@@ -18,7 +45,10 @@ function setup() {
 
   function play() {
       song.play();
+      song.setVolume(1);
+      song.rate(1);
   }
+
 
   function stop() {
       song.stop();
